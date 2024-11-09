d84c562f274dc8929c668322786054c07d197272
SlowedWalk <idetoratojo1@gmail.com>
2024-11-09 15:32:54 +0100

    setup sign up flow

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,17 +1,158 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { createAccount } from "@/lib/actions/users.actions";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+type FormType = "sign-in" | "sign-up";
 
-type AuthFormProps = {
-  type: "sign-in" | "sign-out";
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
 };
 
-const AuthForm = ({ type }: AuthFormProps) => {
-  return <div></div>;
+const AuthForm = ({ type }: { type: FormType }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [accountId, setAccountId] = useState(null);
+
+  const formSchema = authFormSchema(type);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      fullName: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    setIsLoading(true);
+    setErrorMessage("");
+
+    await createAccount({
+      fullName: values.fullName || "",
+      email: values.email,
+    })
+      // .then(console.log)
+      .then(({ accountId }) => setAccountId(accountId))
+      .catch(() => {
+        setErrorMessage("Failed to create account. Please try again.");
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button
+            className="form-submit-button"
+            type="submit"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+
+          {errorMessage && <p className="error-message">*{errorMessage}</p>}
+
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="ml-1 font-medium text-brand"
+            >
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+      {/* OTP Verification */}
+    </>
+  );
 };
 
 export default AuthForm;
---
+++ lib/actions/users.actions.ts
@@ -0,0 +1,71 @@
+"use server";
+
+// **Create account flow**
+// 1. User enters full name and email
+// 2. Check if the user already exists using the mail (we will use this to identify if we still need to create a user document or not)
+// 3. Send OTP to user's email (this will send a secret key for creating a session. the secret key will be sent to the user's account email. If the user's auth account has)
+// 4. Create a new user document if the user is a new user
+// 5. Return the user's accountId that will be used to complete the login
+// 6. Verify OTP and authenticate to login
+
+import { createAdminSession } from "@/lib/appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { ID, Query } from "node-appwrite";
+import { parseStringify } from "@/lib/utils";
+
+const getUserByEmail = async (email: string) => {
+  const { database } = await createAdminSession();
+  const result = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", email)],
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.error(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async (email: string) => {
+  const { account } = await createAdminSession();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+  const accountId = await sendEmailOTP(email);
+  if (!accountId) throw new Error("Account not found");
+
+  if (!existingUser) {
+    const { database } = await createAdminSession();
+    await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://img.freepik.com/vecteurs-premium/avatar-icon002_750950-52.jpg",
+        accountId,
+      },
+    );
+  }
+
+  return parseStringify({ accountId });
+};
